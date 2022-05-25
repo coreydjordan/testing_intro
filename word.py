26976f08e8293353c2172f082c8f09dbdf0551c4
dd-code-immersives <domilamad89@gmail.com>
2022-05-25 21:02:49 +0300

    put vowels as global in class

--- word.py
+++ word.py
@@ -1,8 +1,12 @@
-class TextInfo():  
+class TextInfo(): 
+
+    vowels = ['a', 'e', 'i', 'o', 'u'] 
+
     def count_vowels(self, text):
+        
         v_count = 0
         c_count = 0
-        vowels = ['a', 'e', 'i', 'o', 'u']
+        
         for i in text:
             if i in vowels:
                 v_count += 1
@@ -10,9 +14,10 @@ class TextInfo():
                 c_count += 1
         return v_count       
     def count_cons(self, text):
+
         v_count = 0
         c_count = 0
-        vowels = ['a', 'e', 'i', 'o', 'u']
+        
         for i in text:
             if i in vowels:
                 v_count += 1
